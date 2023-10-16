04bbde7f9386350819b6e7534dd4ffc7c265419d
ausias-armesto <ausiasarmesto@gmail.com>
2023-10-16 17:58:45 +0200

    increase size

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,8 @@ const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('notification-service:app');
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '2mb'}))
 app.use(cors());
-app.use(express.json({limit: '1mb'}));
-app.use(express.urlencoded({limit: '1mb'}));
 routes.push(new AlertManagerRoutes(app));
 
 app.use(expressWinston.errorLogger({
